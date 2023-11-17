cba4564dad1b4c902e310b4aecc741d71cc94b64
Andrew Segales <andysejas444@gmail.com>
2023-11-16 22:45:22 -0400

    Erecion

--- src/components/view3/View3.jsx
+++ src/components/view3/View3.jsx
@@ -1,22 +1,31 @@
 
 import React from 'react';
 import './View3.css';
+import './Direcion.css';
+
 
 const View3 = () => {
   return (
-    <div className="view">
-      <h2>Contacto y Ubicación</h2>
-      <p>¡Visítanos en nuestra pastelería! Contáctanos para hacer pedidos personalizados o para obtener más información sobre nuestros productos.</p>
-      <div className="contact-info">
-        <p>Dirección: Calle de la Pastelería, 123</p>
-        <p>Teléfono: 123-456-7890</p>
-        <p>Email: info@pasteleriadeliciosa.com</p>
-      </div>
-      <div className="map-container">
-        {/* Aquí puedes integrar un mapa interactivo o una imagen del mapa de tu ubicación */}
-        <img src="map.jpg" alt="Mapa de la ubicación de la pastelería" />
-      </div>
-    </div>
+    
+    <article>
+      <section className='caja-Delivery'>
+        <h2 className='Direc-Delivery'>Dirección de Delivery</h2>
+        <form className='form'>
+            <div className='form_group'>
+              <input type="text" id='num' className='form_input' placeholder=' '/>
+              <label htmlFor="name" className='form_label'>Dirección</label>
+            </div>
+        </form>
+        <form className='form'>
+            <div className='form_group'>
+              <input type="numero" id='num' className='form_input' placeholder=' '/>
+              <label htmlFor="name" className='form_label'>Numero</label>
+            </div>
+        </form>
+        </section>
+        <button className='bote'>Pago</button>
+    </article>
+    
   );
 };
 
