c0ae2f820a4886e955ca923d75b8758aab3741bb
Icondoric <iconhu.icc@gmail.com>
2023-11-16 23:03:01 -0400

    p Segales

--- src/components/view3/View3.jsx
+++ src/components/view3/View3.jsx
@@ -6,6 +6,7 @@ import './Direcion.css';
 
 const View3 = () => {
   return (
+<<<<<<< HEAD
     
     <article>
       <section className='caja-Delivery'>
@@ -26,6 +27,25 @@ const View3 = () => {
         <button className='bote'>Pago</button>
     </article>
     
+=======
+    <div className="view">
+      <div className="top-images">
+        <img src="/top/back.png" alt="Back" className="back-image" />
+        <img src="/top/Pastelito.png" alt="Pastelito" className="pastelito-image" />
+      </div>
+      <h2>Pasteles</h2>
+      <p>¡Visítanos en nuestra pastelería! Contáctanos para hacer pedidos personalizados o para obtener más información sobre nuestros productos.</p>
+      <div className="contact-info">
+        <p>Dirección: Calle de la Pastelería, 123</p>
+        <p>Teléfono: 123-456-7890</p>
+        <p>Email: info@pasteleriadeliciosa.com</p>
+      </div>
+      <div className="map-container">
+        {/* Aquí puedes integrar un mapa interactivo o una imagen del mapa de tu ubicación */}
+        <img src="map.jpg" alt="Mapa de la ubicación de la pastelería" />
+      </div>
+    </div>
+>>>>>>> e4a2f21 (Cambio en css)
   );
 };
 
