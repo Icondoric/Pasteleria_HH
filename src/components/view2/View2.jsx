0601cf8d49391b7049cde59dd9af8b1e315ca2ce
Icondoric <iconhu.icc@gmail.com>
2023-11-16 23:55:34 -0400

    vha

--- src/components/view2/View2.jsx
+++ src/components/view2/View2.jsx
@@ -1,38 +1,34 @@
-
+// /src/components/view3/View3.js
 import React from 'react';
 import './View2.css';
+import TagPasteles from '../view2/TagPasteles';
+import { pastel } from '../../assets/PastelesAPI'; // Ajusta la ruta según la ubicación real de tus datos
 
-// const View2 = (props) => {
-//   const pastel=props.pastel;
-//   const theme=cars.theme;
-//   return (
-//     // <div className="view">
-//     //   <h2>Pasteles</h2>
-//     //   <div className="menu-items">
-//     //     <div className="menu-item">
-//     //       <img src="cake2.jpg" alt="Pastel de Vainilla" />
-//     //       <p>Pastel de Vainilla</p>
-//     //       <p className="price">$24.99</p>
-//     //     </div>
-//     //   </div>
-//     // </div>
-
-//   )
-// }
-function View2(props) 
-{
-  const pastel=props.pastel;
-  const theme=pastel.theme;
-  console.log(theme)
-  return(
-    <section className="View">
-      <div className='menu-items'>
-      <img className='menu-item img' src={pastel.imgUrl} alt={pastel.title} />
-      <h2>{pastel.title}</h2>
-      <p>{pastel.description}</p>
+const View2 = () => {
+  return (
+    <div className="view">
+      {/* Sección superior (pasteles) */}
+      <div className="top-section">
+        <div className="top-images">
+          <img src="/public/top/back.png" alt="Botón de Retroceso" className="back-button" />
+          <img src="/public/top/Pastelito.png" alt="Pastelito Logo" className="logo" />
+        </div>
       </div>
-    </section>
-  )
-}
+
+      {/* Sección de la API (pasteles) al final */}
+      <div className="api-section">
+        {pastel.map((pastel, index) => (
+          <TagPasteles key={index} pastel={pastel} />
+        ))}
+      </div>
+
+      {/* Sección "Contacto y Ubicación" */}
+      <div className="bottom-section">
+        <h2>Contacto y Ubicación</h2>
+        {/* ... (contenido de la sección de "Contacto y Ubicación") */}
+      </div>
+    </div>
+  );
+};
 
 export default View2;
